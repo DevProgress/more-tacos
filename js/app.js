f9b56306b5d5ce6c29dea00d383fc82026563d2d
Kimberly Nicholls <kimberly@gridium.com>
2016-09-12 21:00:36 -0700

    a bit more space in save dialog
    fixes https://github.com/DevProgress/more-tacos/issues/92

--- js/app.js
+++ js/app.js
@@ -123,7 +123,7 @@ var TacoMap = function(mapEl, database, initialPosition, initialZoom) {
   this._mc.setCalculator(CONFIG.TACO_MAP.clusterCalculator);
 
   /** @private {google.maps.InfoWindow} Pop-up window for the map. */
-  this._iw = new google.maps.InfoWindow({maxWidth: 250});
+  this._iw = new google.maps.InfoWindow({maxWidth: 270});
 
   /** @private {google.maps.Marker} The user's draggable marker. */
   this._userMarker = new google.maps.Marker({
