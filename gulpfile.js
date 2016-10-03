53ce83236e9ee4db0bff2fd71223f04a4a791e54
Kathryn Killebrew <kkillebrew@azavea.com>
2016-10-03 15:45:19 -0400

    More gulp
    Add tasks to clean and different builds for production and development.

--- gulpfile.js
+++ gulpfile.js
@@ -5,29 +5,51 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var cleancss = require('gulp-clean-css');
+var del = require('del');
 
 var vendorScripts = [
     'node_modules/js-marker-clusterer/src/markerclusterer.js',
     'node_modules/rlite-router/rlite.js'
 ];
 
-var mainScripts = [
-    'js/metrics.js',
-    'js/app.js'
-];
+gulp.task('clean', function() {
+    return del.sync(['dist/**'], {force: true});
+});
 
-gulp.task('vendor', function() {
+gulp.task('minify:vendor', function() {
     return gulp.src(vendorScripts)
-        .pipe(concat('vendor.js'))
         .pipe(uglify())
         .pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('scripts', function() {
-    return gulp.src(mainScripts)
-        .pipe(concat('main.js'))
+gulp.task('minify:scripts', function() {
+    return gulp.src('js/**')
         .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));  
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('copy:vendor', function() {
+    return gulp.src(vendorScripts)
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('copy:scripts', function() {
+    return gulp.src('js/**')
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('minify:images', function() {
+    return gulp.src('images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('copy:css', function() {
+    return gulp.src('css/*')
+        .pipe(cleancss({compatibility: 'ie8'}))
+        .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('watch', function() {
@@ -40,7 +62,11 @@ gulp.task('lint', function() {
         .pipe(jshint.reporter('default'));
 });
 
-gulp.task('build', ['lint', 'vendor', 'scripts']);
-gulp.task('run', ['build', 'watch']);
+gulp.task('build:production', ['clean', 'lint', 'minfy:vendor', 'minify:scripts',
+          'minify:images', 'copy:css']);
+gulp.task('build:development', ['clean', 'lint', 'copy:vendor', 'copy:scripts',
+          'minify:images', 'copy:css']);
 
-gulp.task('default', ['build']);
+gulp.task('run', ['build:development', 'watch']);
+
+gulp.task('default', ['run']);
