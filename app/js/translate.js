eae51883e4da47da8201eda3159e6abdce5f9f4a
Kathryn Killebrew <banderkat@gmail.com>
2016-10-15 16:03:59 -0400

    Handle translating popup

--- app/js/translate.js
+++ app/js/translate.js
@@ -1,8 +1,62 @@
 /* global Polyglot, jQuery */
 
-(function(window, $, Polyglot) {
+TacoTranslator = (function(window, $, Polyglot) {
+    'use strict';
 
     var SUPPORTED_TRANSLATIONS = ['es'];
+
+    var locale = null;
+    var phrases = null;
+    var polyglot = null;
+
+    function TacoTranslator() {}
+
+    TacoTranslator.prototype.initialize = initialize;
+    TacoTranslator.prototype.translatePhraseInElement = translatePhraseInElement;
+
+    return TacoTranslator;
+
+    /**
+     * Initialize translator.
+     *
+     * @return {Object} Promise resolving when translations are done loading.
+     */
+    function initialize() {
+        var deferred = $.Deferred();
+        locale = browserLocale();
+
+        var localeIdx = SUPPORTED_TRANSLATIONS.indexOf(locale);
+        if (localeIdx < 0) {
+            // browser locale not supported; leave untranslated
+            deferred.resolve();
+            return deferred;
+        }
+
+        $.get('js/i18n/' + locale + '.json').then(function(result) {
+            phrases = result;
+            polyglot = new Polyglot({phrases: phrases});
+            translate();
+            deferred.resolve();
+        }, deferred.reject);
+
+        return deferred;
+    }
+
+    /**
+     * Translate an HTML snippet. Expects `translate` to have already been called.
+     *
+     * @param {Object} $el jQuery element to translate
+     * @returns {Object} $el jQuery element, translated 
+     */
+    function translatePhraseInElement($el) {
+        var phraseNames = Object.keys(phrases);
+        
+        for (var i = 0; i < phraseNames.length; i++) {
+            var phraseName = phraseNames[i];
+            $el.find('.i18n-' + phraseName).text(polyglot.t(phraseName));
+        }
+        return $el;
+    }
 
     /**
      * Inspect browser locale, as available to JavaScript.
@@ -29,8 +83,7 @@
      *
      * @param {Object} phrases Dictionary of translations
      */
-    function translate(phrases) {
-        var polyglot = new Polyglot({phrases: phrases});
+    function translate() {
         var phraseNames = Object.keys(phrases);
         
         for (var i = 0; i < phraseNames.length; i++) {
@@ -38,17 +91,5 @@
             $('.i18n-' + phraseName).text(polyglot.t(phraseName));
         }
     }
-
-    var locale = browserLocale();
-
-    var localeIdx = SUPPORTED_TRANSLATIONS.indexOf(locale);
-    if (localeIdx < 0) {
-        // browser locale not supported; leave untranslated
-        return;
-    }
-
-    $.get('js/i18n/' + locale + '.json').then(function(result) {
-        translate(result);
-    });
 
 })(window, jQuery, Polyglot);
